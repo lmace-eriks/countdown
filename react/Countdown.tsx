b26e742b437d827acd56e49baa1505286dba0486
WebDevloper <useraccount@WebDeveloperMBP.local>
2021-12-07 17:01:12 -0600

    Adding <a> and removing close function - LM

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -7,24 +7,24 @@ import { relative } from 'path';
 
 interface CountdownProps {
   targetDate: string,
+  blockLink: string,
   countdownTitle: string,
   countdownTitleColor: string,
   countdownTitleFontSize: string,
   countdownColor: string,
   countdownFontSize: string,
   blockBackgroundColor: string
-  // blockBackgroundImage: string
 }
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   targetDate,
+  blockLink,
   countdownTitle,
   countdownTitleColor,
   countdownTitleFontSize,
   countdownColor,
   countdownFontSize,
   blockBackgroundColor
-  // blockBackgroundImage
 }) => {
 
   const [timeRemaining, setTime] = useState<TimeSplit>({
@@ -35,20 +35,23 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     timeOver: true // Prevents initial loading of countdown if the target time has passed --
   });
 
-  const [userHideAction, setUserHideAction] = useState<boolean>(false);
-
   const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
 
   tick(targetDate, setTime);
 
   // Component Styles. Some values are fed by SiteEdior --
+  const aStyle: CSSProperties = {
+    textDecoration: "none"
+  }
+
   const blockDiv: CSSProperties = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: blockBackgroundColor,
-    width: "100%"
+    width: "100%",
+    cursor: "pointer"
   }
 
   const h1Style: CSSProperties = {
@@ -68,34 +71,17 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     paddingBottom: "0.25rem"
   }
 
-  const closeButtonStyle: CSSProperties = {
-    color: countdownTitleColor,
-    fontSize: "1rem",
-    textAlign: "right",
-    padding: "0",
-    margin: "0",
-    cursor: "pointer",
-    alignSelf: "flex-end",
-    marginRight: "0.75rem",
-    position: "relative",
-    top: "0.75rem",
-    lineHeight: "0"
-  }
-
-  const handleCloseClick = () => {
-    setUserHideAction(true);
-  };
-
-  if (!!timeRemaining.timeOver || userHideAction) {
-    return (<></>) // Block dissapears if Target Date has passed or user clicks [ x ] --
+  if (!!timeRemaining.timeOver) {
+    return (<></>) // Block dissapears if Target Date has passed --
   } else {
     return (
-      <div style={blockDiv}>
-        <p style={closeButtonStyle} onClick={handleCloseClick}>[ x ]</p>
-        <h1 style={h1Style}>{countdownTitle}</h1>
-        {/* Renders "s" if time value is anything other than 1. Prevents "1 minutes"  */}
-        <h2 style={h2Style}>{timeRemaining.days} Day{(timeRemaining.days === "1") ? "" : "s"}, {timeRemaining.hours} Hour{(timeRemaining.hours === "01") ? "" : "s"}, {timeRemaining.minutes} Minute{(timeRemaining.minutes === "01") ? "" : "s"} and {timeRemaining.seconds} Second{(timeRemaining.seconds === "01") ? "" : "s"}</h2>
-      </div>
+      <a style={aStyle} href={blockLink}>
+        <div style={blockDiv}>
+          <h1 style={h1Style}>{countdownTitle}</h1>
+          {/* Renders "s" if time value is anything other than 1. Prevents "1 minutes"  */}
+          <h2 style={h2Style}>{timeRemaining.days} Day{(timeRemaining.days === "1") ? "" : "s"}, {timeRemaining.hours} Hour{(timeRemaining.hours === "01") ? "" : "s"}, {timeRemaining.minutes} Minute{(timeRemaining.minutes === "01") ? "" : "s"} and {timeRemaining.seconds} Second{(timeRemaining.seconds === "01") ? "" : "s"}</h2>
+        </div>
+      </a>
     )
   }
 }
@@ -110,6 +96,12 @@ Countdown.schema = {
       description: "Final date used in the countdown YYYY-MM-DD. Treats date as midnight the morning of date.",
       type: "string",
       default: null
+    },
+    blockLink: {
+      title: "Link",
+      description: "https://www...",
+      type: "string",
+      default: "#"
     },
     countdownTitle: {
       title: "Countdown Title",
