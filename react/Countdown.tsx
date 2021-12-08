bf3d458fe578045c62141aa4c9435d8aa9385c84
WebDevloper <useraccount@WebDeveloperMBP.local>
2021-12-08 10:36:27 -0600

    Second line and anchor tag added - LM

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -11,6 +11,9 @@ interface CountdownProps {
   countdownTitle: string,
   countdownTitleColor: string,
   countdownTitleFontSize: string,
+  titleSecondLine: string,
+  titleSecondLineFontSize: string,
+  titleSecondLineColor: string,
   countdownColor: string,
   countdownFontSize: string,
   blockBackgroundColor: string
@@ -22,6 +25,9 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   countdownTitle,
   countdownTitleColor,
   countdownTitleFontSize,
+  titleSecondLine,
+  titleSecondLineFontSize,
+  titleSecondLineColor,
   countdownColor,
   countdownFontSize,
   blockBackgroundColor
@@ -40,10 +46,13 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   tick(targetDate, setTime);
 
   // Component Styles. Some values are fed by SiteEdior --
+
+  // Wrapping Anchor Tag --
   const aStyle: CSSProperties = {
     textDecoration: "none"
   }
 
+  // Block or Background --
   const blockDiv: CSSProperties = {
     display: "flex",
     flexDirection: "column",
@@ -54,7 +63,8 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     cursor: "pointer"
   }
 
-  const h1Style: CSSProperties = {
+  // Title First Line --
+  const h2Style: CSSProperties = {
     color: countdownTitleColor,
     fontSize: countdownTitleFontSize,
     textAlign: "center",
@@ -62,7 +72,17 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     padding: "0"
   }
 
-  const h2Style: CSSProperties = {
+  // Title Second Line --
+  const h2SecondLineStyle: CSSProperties = {
+    color: titleSecondLineColor || countdownTitleColor,
+    fontSize: titleSecondLineFontSize || countdownTitleFontSize,
+    textAlign: "center",
+    margin: "0",
+    padding: "0"
+  }
+
+  // Countdown --
+  const h3Style: CSSProperties = {
     color: countdownColor,
     fontSize: countdownFontSize,
     textAlign: "center",
@@ -77,9 +97,14 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     return (
       <a style={aStyle} href={blockLink}>
         <div style={blockDiv}>
-          <h1 style={h1Style}>{countdownTitle}</h1>
+          <h2 style={h2Style}>{countdownTitle}</h2>
+
+          {/* Renders only if defined */}
+           {titleSecondLine && <h2 style={h2SecondLineStyle}>{titleSecondLine}</h2>}
+
           {/* Renders "s" if time value is anything other than 1. Prevents "1 minutes"  */}
-          <h2 style={h2Style}>{timeRemaining.days} Day{(timeRemaining.days === "1") ? "" : "s"}, {timeRemaining.hours} Hour{(timeRemaining.hours === "01") ? "" : "s"}, {timeRemaining.minutes} Minute{(timeRemaining.minutes === "01") ? "" : "s"} and {timeRemaining.seconds} Second{(timeRemaining.seconds === "01") ? "" : "s"}</h2>
+          <h3 style={h3Style}>{timeRemaining.days} Day{(timeRemaining.days === "1") ? "" : "s"}, {timeRemaining.hours} Hour{(timeRemaining.hours === "01") ? "" : "s"}, {timeRemaining.minutes} Minute{(timeRemaining.minutes === "01") ? "" : "s"} and {timeRemaining.seconds} Second{(timeRemaining.seconds === "01") ? "" : "s"}</h3>
+        
         </div>
       </a>
     )
@@ -95,44 +120,72 @@ Countdown.schema = {
       title: "Final Date",
       description: "Final date used in the countdown YYYY-MM-DD. Treats date as midnight the morning of date.",
       type: "string",
-      default: null
+      default: "2099-01-01"
     },
+
     blockLink: {
       title: "Link",
-      description: "https://www...",
+      description: "Full path to URL. https://www...",
       type: "string",
       default: "#"
     },
+
     countdownTitle: {
       title: "Countdown Title",
       description: "Free shipping ends in...",
       type: "string",
       default: null
     },
-    countdownTitleColor: {
-      title: "Title Color",
-      description: "red, black, #00FF11",
-      type: "string",
-      default: null
-    },
+
     countdownTitleFontSize: {
       title: "Title Font Size",
       description: "2rem, 2.8rem, 20px",
       type: "string",
       default: null
     },
+
+    countdownTitleColor: {
+      title: "Title Line Color",
+      description: "red, black, #00FF11",
+      type: "string",
+      default: null
+    },
+
+    titleSecondLine: {
+      title: "Title Second Line",
+      description: "Hidden if left blank.",
+      type: "string",
+      default: null
+    },
+
+    titleSecondLineFontSize: {
+      title: "Second Title Font Size",
+      description: "Inherits first line's font size if left blank.",
+      type: "string",
+      default: null
+    },
+
+    titleSecondLineColor: {
+      title: "Second Title Color",
+      description: "Inherits first line's color if left blank.",
+      type: "string",
+      default: null
+    },
+
     countdownColor: {
       title: "Countdown Color",
       description: "red, black, #00FF11",
       type: "string",
       default: null
     },
+
     countdownFontSize: {
       title: "Countdown Font Size",
       description: "2rem, 2.8rem, 20px",
       type: "string",
       default: null
     },
+
     blockBackgroundColor: {
       title: "Background Color",
       description: "black, transparent, #00FF00",
